191acaf2ccb4eb2aab78ebca294a9d51dc22bf74
Skaishgggg <swakaish444@gmail.com>
2024-12-29 12:16:20 +0530

    version2

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
-
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import Wishlist from './pages/Wishlist';
+import Header from './component/Header';
+import Footer from './component/Footer';
 function App() {
   return (
     < >
-      <h1>E cart</h1>
+    <Header/>
+     <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/cart' element={<Cart/>}/>
+      <Route path='/wishlist' element={<Wishlist/>}/>
+
+     </Routes>
+     <Footer/>
     </>
   );
 }
---
+++ src/component/Header.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import {Nav,Navbar,Container, Badge} from 'react-bootstrap';
+import { Link  } from 'react-router-dom';
+function Header() {
+  return (
+    <div >
+       <Navbar expand="lg"  style={{backgroundColor:'#fff8dc' }}>
+      <Container>
+        <Navbar.Brand ><Link to={'/'} style={{color:'black',fontWeight:'bold'}}/><i className="fa-solid fa-truck-fast"></i>E cart</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link  className='btn border' >
+              <Link  className='d-flex align-items-center'  style={{color:'black',fontWeight:'bold',textDecoration:'none'}} to={'/wishlist'}  > 
+              <i className="fa-solid fa-heart text-danger me-2"></i>Whislist
+            <Badge className='ms-2 rounded' bg='light'>9</Badge>
+            </Link>
+            </Nav.Link>
+            <Nav.Link   className='btn border  ms-3'>
+              <Link  style={{color:'black',fontWeight:'bold',textDecoration:'none'}}  className='d-flex align-items-center' to={'/cart'} > <i className="fa-solid fa-cart-shopping text-warning me-2"></i>Cart
+            <Badge className='ms-2 rounded' bg='light'>9</Badge>
+      </Link>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    </div>
+  )
+}
+
+export default Header
